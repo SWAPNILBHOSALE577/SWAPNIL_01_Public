7887684cdda786bebb35d00e1a9b18ad4f1c05c1
SWAPNILBHOSALE577 <swapnilbhosale0507@gmail.com>
2025-07-23 21:06:33 +0530

    Delete first.js

--- first.js
+++
@@ -1,9 +0,0 @@
-function openNewWindow_1() {
-  window.open('mobile.html'); 
-}
-function openNewWindow_2() {
-  window.open('tablet.html'); 
-}
-function openNewWindow_3() {
-  window.open('laptop.html'); 
-}
