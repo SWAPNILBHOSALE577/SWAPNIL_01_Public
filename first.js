2028aaf57427b5e7e5b7aed2c42e1aeb934abe6d
SWAPNILBHOSALE577 <swapnilbhosale0507@gmail.com>
2025-07-23 20:34:39 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,9 @@
+function openNewWindow_1() {
+  window.open('mobile.html'); 
+}
+function openNewWindow_2() {
+  window.open('tablet.html'); 
+}
+function openNewWindow_3() {
+  window.open('laptop.html'); 
+}
---
+++ mobile.js
@@ -0,0 +1,19 @@
+function samsung() {
+  window.open('samsung.html'); 
+}
+
+
+function apple() {
+  window.open('apple.html'); 
+}
+
+
+function google() {
+  window.open('google.html'); 
+}
+
+function redmi() {
+  window.open('redmi.html'); 
+}
+
+
